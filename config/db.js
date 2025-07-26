eaabed64b540b23b67de16b8a0c1909d21d02412
TugasAkhir01 <sta046513@gmail.com>
2025-07-26 10:47:17 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
@@ -8,14 +9,14 @@ const db = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
-console.log('Connecting to DB with:', {
+console.log("Connecting to DB with:", {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
