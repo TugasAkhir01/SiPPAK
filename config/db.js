dd5fc990e64ecd7c17aadb7349882cea84ed569c
TugasAkhir01 <sta046513@gmail.com>
2025-07-26 10:02:07 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,18 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('❌ DB Error:', err.message);
+  } else {
+    console.log('✅ Connected to Railway DB!');
+  }
 });
 
 module.exports = db;
