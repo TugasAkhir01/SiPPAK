78145c0278a7ddf305cc6d033c241e00d064e9e5
TugasAkhir01 <sta046513@gmail.com>
2025-07-26 13:56:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,10 +26,12 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
+  optionsSuccessStatus: 200
 };
 
+const app = express();
 app.use(cors(corsOptions));
-app.use(express.json());
+app.options('*', cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
