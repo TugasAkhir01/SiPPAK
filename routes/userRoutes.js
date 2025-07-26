53e9eb02d30429a7ebcfb054e59cb14f90032c9a
TugasAkhir01 <sta046513@gmail.com>
2025-07-26 15:02:55 +0700

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,15 @@ const { verifyToken } = require('../middleware/authMiddleware');
 const uploadProfile = require('../middleware/uploadProfileMiddleware');
 
 router.get('/', verifyToken, userController.getUsers);
-router.get('/:id', verifyToken, userController.getUser);
-router.post('/add', verifyToken, userController.addUserManagement);
-router.put('/:id', verifyToken, userController.updateUserManagement);
-router.post('/register', verifyToken, uploadProfile.single('photo'), userController.register);
-router.delete('/:id', verifyToken, userController.deleteUser);
+
 router.put('/profile/:id', verifyToken, userController.updateUser);
 router.put('/profile/:id/photo', verifyToken, uploadProfile.single('photo'), userController.updateUserPhoto);
+
+router.post('/add', verifyToken, userController.addUserManagement);
+router.put('/:id', verifyToken, userController.updateUserManagement);
+router.get('/:id', verifyToken, userController.getUser);
+router.delete('/:id', verifyToken, userController.deleteUser);
+
+router.post('/register', verifyToken, uploadProfile.single('photo'), userController.register);
 
 module.exports = router;
