fc28a5c973544b468ac361657cbe53bfbb64188b
TugasAkhir01 <sta046513@gmail.com>
2025-07-26 14:07:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/report', express.static(path.join(__dirname, 'exports')));
+app.use('/exports', express.static(path.join(__dirname, 'exports')));
 app.use('/api', violationRoutes);
 
 connectToDB();
