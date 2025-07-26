913f8012543ad6066e2aad9b0ddb9f22f1d307a9
TugasAkhir01 <sta046513@gmail.com>
2025-07-26 17:01:28 +0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,6 +3,12 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const SECRET_KEY = process.env.SECRET_KEY;
+
+if (!SECRET_KEY) {
+  console.warn('⚠️ SECRET_KEY tidak tersedia! Periksa .env atau Railway Variables.');
+}
+
 exports.verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
 
@@ -12,7 +18,7 @@ exports.verifyToken = (req, res, next) => {
 
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
         if (err) return res.status(401).json({ message: 'Token tidak valid' });
         req.user = decoded;
         next();
