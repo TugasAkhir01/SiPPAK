0947ea2276de74c3d57d91eb5f0042a56932078f
TugasAkhir01 <sta046513@gmail.com>
2025-07-26 16:36:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const reportRoutes = require('./routes/reports');
 const { connectToDB } = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 const uploadDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadDir)) {
